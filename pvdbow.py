a2c865c32ed32b3668e44d500379d466a9186a50
Richard Berendsen <richard.berendsen@luminis.eu>
2017-02-02 14:32:47 +0100

    Also feed test data in batches
    This required a bit of refactoring, too; e.g., generate_batch now
    is agnostic to whether it is fed training or test data. It now just
    uses an iterator of text window center positions. If it is training
    data, this generator is eternal, and reshuffles every X epochs. If
    it is testing data, the iterator will run out as usual.

--- pvdbow.py
+++ pvdbow.py
@@ -28,10 +28,32 @@ NUM_STEPS = 10001
 LEARNING_RATE = 0.1
 NUM_SAMPLED = 64
 REPORT_EVERY_X_STEPS = 200
+SHUFFLE_EVERY_X_EPOCH = 5
 
 # Token integer ids for special tokens
 UNK = 0
 NULL = 1
+
+# <codecell>
+
+%load_ext line_profiler
+
+# <codecell>
+
+"""
+Returns an eternal generator, periodically shuffling the order
+
+l_ is a list of integers; an internal copy of it is maintained.
+"""
+def repeater_shuffler(l_):
+    l = np.array(l_, dtype=np.int32)
+    epoch = 0
+    while epoch >= 0:
+        if epoch % SHUFFLE_EVERY_X_EPOCH == 0:
+            np.random.shuffle(l)
+        for i in l:
+            yield i
+        epoch += 1
 
 # <codecell>
 
@@ -146,15 +168,16 @@ def get_train_test():
             twcp_test.append(i)
         else:
             twcp_train.append(i)
-    if not twcp_test:
+    if len(twcp_test) < (BATCH_SIZE // TEXT_WINDOW_SIZE):
         raise ValueError(
-            'No test data, try increasing PV_TEST_SET_PERCENTAGE')
+            'Too little test data, try increasing PV_TEST_SET_PERCENTAGE')
     return twcp_train, twcp_test
 
 # <codecell>
 
 np.random.shuffle(twcp)
 twcp_train, twcp_test = get_train_test()
+twcp_train_gen = repeater_shuffler(twcp_train)
 del twcp # save some memory
 
 # <codecell>
@@ -165,11 +188,9 @@ print('Effective test set percentage: {} out of {}, {:.1f}%'.format(
 
 # <codecell>
 
-np.random.shuffle(twcp_train)
+del twcp_train # save some memory, we use twcp_train_gen from now on
 
 # <codecell>
-
-twcp_train_index = 0
 
 def generate_batch_single_twcp(twcp, i, batch, labels):
     tw_start = twcp - (TEXT_WINDOW_SIZE - 1) // 2
@@ -180,31 +201,12 @@ def generate_batch_single_twcp(twcp, i, batch, labels):
     batch[batch_slice] = docids
     labels[batch_slice, 0] = wordids
     
-def generate_batch():
-    global twcp_train_index
+def generate_batch(twcp_gen):
     batch = np.ndarray(shape=(BATCH_SIZE,), dtype=np.int32)
     labels = np.ndarray(shape=(BATCH_SIZE, 1), dtype=np.int32)
     for i in range(BATCH_SIZE // TEXT_WINDOW_SIZE):
-        generate_batch_single_twcp(twcp_train[twcp_train_index],
-                                   i, batch, labels)
-        twcp_train_index = (twcp_train_index + TEXT_WINDOW_SIZE) \
-                % len(twcp_train)
+        generate_batch_single_twcp(next(twcp_gen), i, batch, labels)
     return batch, labels
-
-# <codecell>
-
-batch, labels = generate_batch()
-
-# <codecell>
-
-test_dataset_ = np.ndarray(shape=(len(twcp_test) * TEXT_WINDOW_SIZE,),
-                          dtype=np.int32)
-test_labels_ = np.ndarray(shape=(len(twcp_test) * TEXT_WINDOW_SIZE,
-                                1),
-                         dtype=np.int32)
-for i in range(len(twcp_test)):
-    generate_batch_single_twcp(twcp_test[i], i, test_dataset_, 
-                              test_labels_)
 
 # <codecell>
 
@@ -213,10 +215,8 @@ graph = tf.Graph()
 with graph.as_default(), tf.device('/cpu:0'):
     
     # Input data
-    train_dataset = tf.placeholder(tf.int32, shape=[BATCH_SIZE])
-    train_labels = tf.placeholder(tf.int32, shape=[BATCH_SIZE, 1])
-    test_dataset = tf.constant(test_dataset_, dtype=tf.int32)
-    test_labels = tf.constant(test_labels_, dtype=tf.int32)
+    dataset = tf.placeholder(tf.int32, shape=[BATCH_SIZE])
+    labels = tf.placeholder(tf.int32, shape=[BATCH_SIZE, 1])
     
     # Weights
     embeddings = tf.Variable(
@@ -230,25 +230,24 @@ with graph.as_default(), tf.device('/cpu:0'):
     
     # Model
     # Look up embeddings for inputs
-    embed = tf.nn.embedding_lookup(embeddings, train_dataset)
+    embed = tf.nn.embedding_lookup(embeddings, dataset)
     # Compute the softmax loss, using a sample of the negative
     # labels each time
     loss = tf.reduce_mean(
             tf.nn.sampled_softmax_loss(
                     softmax_weights, softmax_biases, embed,
-                    train_labels, NUM_SAMPLED, vocab_size))
+                    labels, NUM_SAMPLED, vocab_size))
     
     # Optimizer
     optimizer = tf.train.AdagradOptimizer(LEARNING_RATE).minimize(
             loss)
     
     # Test loss
-    test_embed = tf.nn.embedding_lookup(embeddings, test_dataset)
     test_loss = tf.reduce_mean(
             tf.nn.sparse_softmax_cross_entropy_with_logits(
-                    tf.matmul(test_embed, tf.transpose(
+                    tf.matmul(embed, tf.transpose(
                               softmax_weights)) + softmax_biases,
-                    test_labels[:, 0]))
+                    labels[:, 0]))
     
     # Normalized embeddings (to use cosine similarity later on)
     norm = tf.sqrt(tf.reduce_sum(tf.square(embeddings), 1,
@@ -257,28 +256,50 @@ with graph.as_default(), tf.device('/cpu:0'):
 
 # <codecell>
 
-with tf.Session(graph=graph) as session:
-    session.run(tf.global_variables_initializer())
-    print('Initialized')
-    avg_training_loss = 0
-    for step in range(NUM_STEPS):
-        batch_data, batch_labels = generate_batch()
-        feed_dict = {train_dataset: batch_data,
-                     train_labels: batch_labels}
-        _, l = session.run([optimizer, loss], feed_dict=feed_dict)
-        avg_training_loss += l
-        if step % REPORT_EVERY_X_STEPS == 0:
-            if step > 0:
-                avg_training_loss = \
-                        avg_training_loss / REPORT_EVERY_X_STEPS
-            # The average loss is an estimate of the loss over the
-            # last REPORT_EVERY_X_STEPS batches
-            print('Average loss at step {:d}: {:.1f}'.format(
-                    step, avg_training_loss))
-            avg_training_loss = 0
-            test_l = test_loss.eval()
-            print('Test loss at step {:d}: {:.1f}'.format(
-                    step, test_l))
+def get_test_loss(session):
+    # We do this in batches, too, to keep memory usage low.
+    # Since our graph works with a fixed batch size, we
+    # are lazy and just compute test loss on all batches that
+    # fit in the test set.
+    twcp_test_gen = (i for i in twcp_test)
+    n_batches = (len(twcp_test) * TEXT_WINDOW_SIZE) // BATCH_SIZE
+    test_l = 0.0
+    for _ in range(n_batches):
+        batch_data, batch_labels = generate_batch(twcp_test_gen)
+        test_l += session.run([test_loss], feed_dict={
+                dataset: batch_data, labels: batch_labels
+            })[0]
+    return test_l / n_batches
+
+# <codecell>
+
+def train():
+    with tf.Session(graph=graph) as session:
+        session.run(tf.global_variables_initializer())
+        print('Initialized')
+        avg_training_loss = 0
+        for step in range(NUM_STEPS):
+            batch_data, batch_labels = generate_batch(twcp_train_gen)
+            _, l = session.run(
+                    [optimizer, loss],
+                    feed_dict={dataset: batch_data, labels: batch_labels})
+            avg_training_loss += l
+            if step % REPORT_EVERY_X_STEPS == 0:
+                if step > 0:
+                    avg_training_loss = \
+                            avg_training_loss / REPORT_EVERY_X_STEPS
+                # The average loss is an estimate of the loss over the
+                # last REPORT_EVERY_X_STEPS batches
+                print('Average loss at step {:d}: {:.1f}'.format(
+                        step, avg_training_loss))
+                avg_training_loss = 0
+                test_l = get_test_loss(session)
+                print('Test loss at step {:d}: {:.1f}'.format(
+                        step, test_l))
+
+# <codecell>
+
+%lprun -f train train()
 
 # <codecell>
 
